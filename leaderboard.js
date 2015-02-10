61e2af81c52d4f8e269b547c194168c3a392b5a1
Vijay Ananth <vijay.hassan@gmail.com>
2015-02-10 15:02:53 +0530

    Modern way of using Helpers

--- leaderboard.js
+++ leaderboard.js
@@ -2,9 +2,12 @@ PlayerList = new Mongo.Collection('players')
 console.log('Hello World! -from console log');
 if(Meteor.isClient) {
   console.log('Hello Client! -from console log');
-  Template.leaderboard.player = function() {
-      return "Some Other Text from Leaderboared.player";
-  }
+  Template.leaderboard.helpers({
+    'player' : function() {
+      return "Some Other Text from Leaderboared.helpers.player";
+    },
+    'manager' : "An inspirational quote from Team Manager"
+  });
 }
 
 if (Meteor.isServer) {
