5bcba056b5c444ee5c50a2d13ee7d31f088d1110
Vijay Ananth <vijay.hassan@gmail.com>
2015-02-10 14:40:15 +0530

    Using depricated way of adding helper functions.

--- leaderboard.js
+++ leaderboard.js
@@ -2,6 +2,9 @@ PlayerList = new Mongo.Collection('players')
 console.log('Hello World! -from console log');
 if(Meteor.isClient) {
   console.log('Hello Client! -from console log');
+  Template.leaderboard.player = function() {
+      return "Some Other Text from Leaderboared.player";
+  }
 }
 
 if (Meteor.isServer) {
