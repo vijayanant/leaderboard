5d91e19a856f78d31b7834f746452dab56d43f8d
Vijay Ananth <vijay.hassan@gmail.com>
2015-02-10 16:31:30 +0530

    Sessions and Event data

--- leaderboard.js
+++ leaderboard.js
@@ -5,12 +5,22 @@ if(Meteor.isClient) {
   Template.leaderboard.helpers({
     'player' : function() {
       return PlayerList.find()
+    },
+    'selectedClass' : function () {
+        var playerId = this._id
+        var selectedPlayer = Session.get('selectedPlayer')
+        if (playerId == selectedPlayer) {
+          return 'selected'
+        }
     }
   });
 
   Template.leaderboard.events({
       'click .player' : function() {
-          console.log('click happened');
+          var playerId = this._id
+          Session.set('selectedPlayer', playerId)
+          var selectedPlayer = Session.get('selectedPlayer')
+          console.log(selectedPlayer);
       }
   })
 }
