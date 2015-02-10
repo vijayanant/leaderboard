aa772365b6c484c972bb894d802284b50fe0fbfc
Vijay Ananth <vijay.hassan@gmail.com>
2015-02-10 14:29:25 +0530

    Client and Server Code

--- leaderboard.js
+++ leaderboard.js
@@ -1,2 +1,9 @@
 PlayerList = new Mongo.Collection('players')
-console.log('Hello from console log');
+console.log('Hello World! -from console log');
+if(Meteor.isClient) {
+  console.log('Hello Client! -from console log');
+}
+
+if (Meteor.isServer) {
+  console.log('Hello Server! -from console log');
+}
